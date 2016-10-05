ed9ca85414dfaaadd3431b0d798f95f04e9def0f
Robin Thrift <services@robinthrift.com>
2016-10-05 22:02:44 +0200

    feat(RandomInt): add random int generator

---
+++ commands/random.go
@@ -0,0 +1,41 @@
+package commands
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"math/rand"
+	"github.com/nlopes/slack"
+)
+
+var pattern *regexp.Regexp = regexp.MustCompile("randInt (-?[0-9]+) (-?[0-9]+)")
+
+func randomIndHandler(text string, user *slack.User) (error, string) {
+	intStrs := pattern.FindStringSubmatch(text)
+
+	// @TODO: this could be nicer
+	min, err := strconv.Atoi(intStrs[1])
+	if err != nil {
+		return err, ""
+	}
+
+	// @TODO: this could be nicer
+	max, err := strconv.Atoi(intStrs[2])
+	if err != nil {
+		return err, ""
+	}
+
+	// @TODO: maybe don't hardcode messages?
+	if (min >= max) {
+		return nil, "Error, the second argument must be greater than the first!"
+	}
+
+	// @TODO: maybe don't hardcode messages?
+	return nil, fmt.Sprintf("Your random integer, good sir/madam: %d", rand.Intn(max - min) + min)
+}
+
+var RandomIntCommand = Command{
+	Name: "RandomInt",
+	pattern: pattern,
+	handler: randomIndHandler,
+}
